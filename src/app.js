21eab3b78bc36cf2563d7c6cf313d9dc23a1deec
Geetansh431 <geetanshg2@gmail.com>
2024-10-30 22:42:48 +0530

    Temporary User Routes and Controllers

--- src/app.js
+++ src/app.js
@@ -12,17 +12,21 @@ app.use(
 );
 
 app.use(express.json({ limit: "16kb " })); // configure experss app to parse json data
-
 // url encode krta h jaise kabhi kch search krte ho toh space ki jagah %20 aa jata h woh sab samajhne ke liye
-app.use(
-  express.urlencoded({
-    extended: true,
-    limit: "16kb",
-  })
-);
+app.use(express.urlencoded({extended: true,limit: "16kb",}));
+app.use(express.static("public")); //This setup makes it easier to organize and serve frontend assets directly, without setting up specific routes for each static file.
+app.use(cookieParser());
 
-app.use(express.static("public"))  //This setup makes it easier to organize and serve frontend assets directly, without setting up specific routes for each static file.
-app.use(cookieParser())
+
+//ROUTES import
+
+import  userRouter  from "./routes/user.routes.js";
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter)  // http://localhost:8000/api/v1/users/register
+
+
 
 
 export { app };
