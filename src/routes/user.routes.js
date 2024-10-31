72efb8b0d38231bc73c42a0daef44c3e50e9410e
Geetansh431 <geetanshg2@gmail.com>
2024-10-31 20:59:35 +0530

    Added RefreshTokenEndpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,10 +3,10 @@ import {
   loginUser,
   registerUser,
   logoutUser,
+  refreshAccessToken,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer. middleware.js";
-import {verifyJWT} from "../middlewares/auth.middleware.js";
-
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -28,4 +28,5 @@ router.route("/login").post(loginUser);
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 export default router;
