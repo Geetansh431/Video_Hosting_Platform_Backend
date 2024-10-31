347acbcac49ff0d2871987adac7678b6bbc69e96
Geetansh431 <geetanshg2@gmail.com>
2024-10-31 08:54:23 +0530

    Added User Controller and Bug fixes

--- src/app.js
+++ src/app.js
@@ -11,9 +11,9 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "16kb " })); // configure experss app to parse json data
+app.use(express.json({ limit: "10mb" })); // configure experss app to parse json data
 // url encode krta h jaise kabhi kch search krte ho toh space ki jagah %20 aa jata h woh sab samajhne ke liye
-app.use(express.urlencoded({extended: true,limit: "16kb",}));
+app.use(express.urlencoded({extended: true,limit: "10mb",}));
 app.use(express.static("public")); //This setup makes it easier to organize and serve frontend assets directly, without setting up specific routes for each static file.
 app.use(cookieParser());
 
