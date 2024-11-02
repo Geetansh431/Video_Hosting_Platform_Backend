be60f7b81607419978f359dfd183ab507b760d45
Geetansh431 <geetanshg2@gmail.com>
2024-11-02 15:37:13 +0530

    Added Subscription routes controller models

--- src/app.js
+++ src/app.js
@@ -20,24 +20,24 @@ app.use(cookieParser());
 //ROUTES import
 
 import userRouter from "./routes/user.routes.js";
-import healthcheckRouter from "./routes/healthcheck.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
-import subscriptionRouter from "./routes/subscription.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js";
 import commentRouter from "./routes/comment.routes.js";
-import likeRouter from "./routes/like.routes.js"
-import playlistRouter from "./routes/playlist.routes.js"
-import dashboardRouter from "./routes/dashboard.routes.js"
-import videoRouter from "./routes/video.routes.js"
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
+import videoRouter from "./routes/video.routes.js";
 
 //routes declaration
-app.use("/api/v1/healthcheck", healthcheckRouter) // done
+app.use("/api/v1/healthcheck", healthcheckRouter); // done
 app.use("/api/v1/users", userRouter); // http://localhost:8000/api/v1/users/register
 app.use("/api/v1/tweets", tweetRouter); // done
-app.use("/api/v1/subscriptions", subscriptionRouter) 
-app.use("/api/v1/videos", videoRouter) // done
-app.use("/api/v1/comments", commentRouter); 
-app.use("/api/v1/likes", likeRouter)
-app.use("/api/v1/playlist", playlistRouter)
-app.use("/api/v1/dashboard", dashboardRouter) 
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter); // done
+app.use("/api/v1/comments", commentRouter); // done
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 export { app };
